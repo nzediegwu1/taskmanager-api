161405eddeb2489ea9a433e2dbc47df7eb77dbfa
Anaeze Nsoffor <andeladeveloper@Andelas-MacBook-Pro-22.local>
2018-08-12 03:30:07 +0100

    fix(refactor-login): refactor login implementation
    1. integrate flask_restful
    2. remove jsonify
    3. remove app.debug setting
    4. remove redundant count and response variables
    
    [Finishes refactor-login]

--- app.py
+++ app.py
@@ -1,57 +1,61 @@
-from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token
-from dotenv import load_dotenv
 import os
 import re
+
+from flask import Flask
+from flask_jwt_extended import JWTManager, create_access_token
+from dotenv import load_dotenv
+from flask_restful import Resource, Api, request
 
 from db.users import users
 
 load_dotenv()
 app = Flask(__name__)
+api = Api(app)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 EMAIL_REGEX = re.compile(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$")
 
 
-@app.route('/')
-def hello():
-    return "Hello world, this is Simple-todo app from flask"
+class HelloWorld(Resource):
+    def get(self):
+        return "Hello world, this is Simple-todo app from flask"
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or not EMAIL_REGEX.match(email):
-        message = 'email not provided or invalid'
-        return jsonify({'status': 'error', 'message': message}), 400
-    elif password is None or len(password) < 6:
-        message = 'password not provided or invalid'
-        return jsonify({'status': 'error', 'message': message}), 400
-    else:
-        count = 0
-        response = None
-        for user in users:
-            if user['email'] == email:
-                count += 1
-                if user['email'] == email and user['password'] == password:
-                    response = {
-                        'status': 'success',
-                        'data': user,
-                        'message': 'login successful',
-                        'token': create_access_token(user['id'])
-                    }
+class Login(Resource):
+    def post(self):
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email is None or not EMAIL_REGEX.match(email):
+            message = 'Email not provided or invalid'
+            return {'status': 'error', 'message': message}, 400
+        elif password is None or len(password) < 6:
+            message = 'Password not provided or invalid format'
+            return {'status': 'error', 'message': message}, 400
+        else:
+            visitor = None
+            for user in users:
+                if user['email'] == email:
+                    visitor = user
                     break
-        if count:
-            if response:
-                return jsonify(response)
+            if not visitor:
+                message = 'User does not exist'
+                return {'status': 'error', 'message': message}, 403
+            elif password != visitor['password']:
+                message = 'Invalid login details'
+                return {'status': 'error', 'message': message}, 401
             else:
-                data = {'status': 'error', 'message': 'invalid login details'}
-                return jsonify(data), 401
-        data = {'status': 'error', 'message': 'user does not exist'}
-        return jsonify(data), 403
+                current_user = {**visitor}
+                del current_user['password']
+                return {
+                    'status': 'success',
+                    'data': current_user,
+                    'message': 'Login successful',
+                    'token': create_access_token(current_user['id'])
+                }
 
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
-    app.debug = True
     app.run('localhost', 3000)
