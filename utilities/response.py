1ffa2c88c9e530c27580cb885fe6cc489ee38e22
Anaeze Nsoffor <andeladeveloper@Andelas-MacBook-Pro-22.local>
2018-08-21 16:27:01 +0100

    fix(integrate-mashmallow): replace custom validations with mashmallow
    1. create success response reusable resource
    2. create UserSchema to validate user data using mashmallow
    3. modify signup and login implementations to use mashmallow
    
    [Finishes integrate-mashmallow]

--- utilities/response.py
+++ utilities/response.py
@@ -1,3 +1,6 @@
 class Response():
     def error_resp(self, message, code):
         return {'status': 'error', 'message': message}, code
+
+    def succcess_resp(self, message, data, code):
+        return {'status': 'success', 'data': data, 'message': message}, code
