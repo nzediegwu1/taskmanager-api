69613fe4e465af2dba392997274c6bf447f30ae1
anaeze <nzediegwu1@gmail.com>
2018-11-12 00:46:47 +0100

    chore(database-migration): integrate flask migration
    - install flask-script and flask-migrate for alembic migration command
    - install psycopg2 to connect sqlalchemy with postgres database
    - initialize migration using command: `flask db init`
    - generate first migration for user model using `flask db migrate`
    
    [Maintains #161865059]

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
 """Module for handling application setup"""
 import os
 
+from flask_migrate import MigrateCommand, Migrate
+from flask_script import Manager
 from flask import Flask, Blueprint, jsonify
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
-from flask_restful import Resource, Api
+from flask_restful import Api
 
 from models.database import db
 from views.users import LoginResource, UserResource
+from index import Index
 from config import configure_app
-from utilities.response import success
 from utilities.exceptions.ValidationError import (ValidationError,
                                                   error_blueprint)
 
+BASE_URL = '/api/v1'
 load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 jwt = JWTManager(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
 
-class HelloWorld(Resource):
-    """Root endpoint"""
+manager.add_command('db', MigrateCommand)
 
-    def get(self):
-        message = 'Welcome to Task-Manager api from flask!'
-        return success(message)
-
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(UserResource, '/users')
-api.add_resource(LoginResource, '/login')
+api.add_resource(Index, '/')
+api.add_resource(UserResource, f'{BASE_URL}/users')
+api.add_resource(LoginResource, f'{BASE_URL}/login')
 
 
 @error_blueprint.app_errorhandler(ValidationError)
